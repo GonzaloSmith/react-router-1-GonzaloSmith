413e5777961ed8cc1e3b7f5261cbe9971bd9208b
GonzaloSmith <gonzalosmithveliz@gmail.com>
2022-09-15 16:32:27 -0300

    Se agrega proyecto

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        Gonzalo Smith - 2022 Todos los derechos reservados.
+    </div>
+  )
+}
+
+export default Footer
